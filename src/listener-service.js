00c08291a4cf3c88e5d5fb1f9c8eb41647fc2eea
harisahmar <haris.ahmar@firstcry.in>
2025-04-26 18:22:40 +0530

    Finalize changes

--- src/listener-service.js
+++ src/listener-service.js
@@ -3,16 +3,21 @@ const pgModel = require('./model');
 const redisClient = interactor.redisClient;
 
 async function subscriberService(){
-    redisClient.subscribe('user_created', async (message) => {
-        const data = JSON.parse(message);
-        const modified_at = new Date();
-        const insertIntoPG = await pgModel.insertIntoUserModified(data, modified_at);
-        if(insertIntoPG.rows.length){
-            console.log('User copied to users_modified:', data.id);
-        }else{
-            console.log("Data Not Found");
-        }
-      });
+    try{
+        redisClient.subscribe('user_created', async (message) => {
+            const data = JSON.parse(message);
+            const modified_at = new Date();
+            const insertIntoPG = await pgModel.insertIntoUserModified(data, modified_at);
+            if(insertIntoPG.rows.length){
+                console.log('User copied to users_modified:', data.id);
+            }else{
+                console.log("Data Not Found");
+            }
+          });
+    }catch(err){
+        console.log(err);
+    }
+    
 }
 
 subscriberService();
